0a9fff159439f84666f10c77417800edc0b1a6e7
ebadiere <ebadiere@gmail.com>
2026-04-11 12:10:31 -0600

    feat: add GET /metrics/summary endpoint with tests

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import transactionsRouter from "./routes/transactions";
+import transactionsRouter from "./routes/transactions.js";
+import metricsRouter from "./routes/metrics.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/transactions", transactionsRouter);
+app.use("/metrics", metricsRouter);
 
 export default app;
